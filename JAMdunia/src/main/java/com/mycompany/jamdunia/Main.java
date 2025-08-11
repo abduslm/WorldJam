b89c6d1f5fd0f6f209059d4331cd931d528aac8c
abduslm <sabdusi222@gmail.com>
2025-08-11 09:41:07 +0700

    Update pull Main.java

--- JAMdunia/src/main/java/com/mycompany/jamdunia/Main.java
+++ JAMdunia/src/main/java/com/mycompany/jamdunia/Main.java
@@ -1,3 +1,5 @@
+//git pull
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -18,3 +20,4 @@ public class Main {
     }
 
 }
+
